5cceae5ecb410dc8ef3b0969f22a98b1a92dff09
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-02-01 01:11:07 +0500

    making databse models using mongoose

--- backend/middlewares/user.middlware.js
+++
@@ -1,9 +0,0 @@
-const function1 = (req, res, next) => {
-  console.log("Middleware function1 is executed.");
-  next();
-};
-const function2 = (req, res, next) => {
-  console.log("Middleware function2 is executed.");
-  next();
-};
-export { function1, function2 };
